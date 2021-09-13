938dd971a79c202ddba9740603d51abc91d60b71
TforTrevor <36804391+TforTrevor@users.noreply.github.com>
2021-09-13 15:07:11 -0400

    import code

---
+++ src/edu/ufl/cise/plpfa21/assignment1/CompilerComponentFactory.java
@@ -0,0 +1,11 @@
+package edu.ufl.cise.plpfa21.assignment1;
+
+public class CompilerComponentFactory {
+
+	static IPLPLexer getLexer(String input) {
+		//TODO  create and return a Lexer instance to parse the given input.
+		return null;
+	}
+	
+
+}
---
+++ src/edu/ufl/cise/plpfa21/assignment1/IPLPLexer.java
@@ -0,0 +1,6 @@
+package edu.ufl.cise.plpfa21.assignment1;
+
+public interface IPLPLexer {
+	
+	IPLPToken nextToken() throws LexicalException;
+}
